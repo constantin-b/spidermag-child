9081bc69719e9a6bbd21e8c88359c626c64552e3
Constantin <constantin.b@gmail.com>
2021-02-23 17:46:35 +0200

    Added the other image templates

---
+++ template-parts/featured-image-standard.php
@@ -0,0 +1,11 @@
+<div class="col-sm-16">
+	<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID() ), 'spidermag-featured-image', true ); ?>
+	<div class="standardpost" style="background-image:url('<?php echo esc_url( $image[0] ); ?>'); ">
+		<div class="stanterpost-single-post">
+			<h3> <?php the_title(); ?></h3>
+			<div class="text-danger sub-info-bordered">
+				<?php spidermag_single_post_meta(); ?>
+			</div>
+		</div> <!-- /stanterpost-single-post -->
+	</div><!---/ standard -->
+</div>  <!---/ col-sm-16 -->
