61a319bc8510277fae7edc4583ea8d43361f0654
Constantin <constantin.b@gmail.com>
2021-02-23 17:46:56 +0200

    Updated template for the featured video to use CSS class

--- template-parts/featured-video.php
+++ template-parts/featured-video.php
@@ -4,10 +4,14 @@
  * @project videographywp-pro
  */
 
+/**
+ * @var array $args Array of arguments passed to the template.
+ */
+
 // enable lazy load by default
 add_filter( 'cvwp_lazy_load', '__return_true' );
 // embed the video
-cvwp_embed_video( '<div class="thumb-box">', '</div>' );
+cvwp_embed_video( sprintf( '<div class="thumb-box %s">', $args['class'] ), '</div>' );
 ?>
 <div class="stanterpost-single-post">
 	<h3> <?php the_title(); ?></h3>
